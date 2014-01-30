9e3a42ada71df2246077ea7084f8909f72b70f5e
Barry Kooij <git@barrykooij.nl>
2014-01-30 15:06:31 +0100

    Code style change

--- admin-restriction.php
+++ admin-restriction.php
@@ -7,23 +7,20 @@ Version: 1.1.2
 Author: Barry Kooij
 Author URI: http://www.barrykooij.com/
 */
-require_once('ARHooks.php');
-class AdminRestriction 
-{
-  public function __construct()
-  {
-    $this->setup();
-  }
-  
-  public function setup()
-  {
-    add_action('admin_menu', array(ARHooks::get_instance(), 'disallow_editor'));
-    add_filter('pre_site_transient_update_core', array(ARHooks::get_instance(), 'disallow_core_update'));
-    add_filter('plugin_action_links', array(ARHooks::get_instance(), 'hide_plugin_controls'), 10, 4);
-    add_filter('user_has_cap', array(ARHooks::get_instance(), 'catch_plugin_cap'), 10, 3);
-  }
-  
+require_once( 'class-hooks.php' );
+
+class Admin_Restriction {
+	public function __construct() {
+		$this->setup();
+	}
+
+	public function setup() {
+		add_action( 'admin_menu', array( AR_Hooks::get_instance(), 'disallow_editor' ) );
+		add_filter( 'pre_site_transient_update_core', array( AR_Hooks::get_instance(), 'disallow_core_update' ) );
+		add_filter( 'plugin_action_links', array( AR_Hooks::get_instance(), 'hide_plugin_controls' ), 10, 4 );
+		add_filter( 'user_has_cap', array( AR_Hooks::get_instance(), 'catch_plugin_cap' ), 10, 3 );
+	}
+
 }
 
-new AdminRestriction();
-?>
+new Admin_Restriction();
